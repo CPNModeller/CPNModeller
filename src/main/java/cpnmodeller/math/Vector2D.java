1662199bca4dac4d5c39f5f2f6fd1ea0bb7fc250
Maarten Flippo <maartenflippo@outlook.com>
2018-11-21 15:20:00 +0100

    Mark parameters final in vector class

--- src/main/java/cpnmodeller/math/Vector2D.java
+++ src/main/java/cpnmodeller/math/Vector2D.java
@@ -5,33 +5,33 @@ public final class Vector2D {
     public final float x;
     public final float y;
 
-    public Vector2D(float x, float y) {
+    public Vector2D(final float x, final float y) {
         this.x = x;
         this.y = y;
     }
 
-    public Vector2D add(Vector2D other) {
+    public Vector2D add(final Vector2D other) {
         return new Vector2D(
                 x + other.x,
                 y + other.y
         );
     }
 
-    public Vector2D subtract(Vector2D other) {
+    public Vector2D subtract(final Vector2D other) {
         return new Vector2D(
                 x - other.x,
                 y - other.y
         );
     }
 
-    public Vector2D multiply(Vector2D other) {
+    public Vector2D multiply(final Vector2D other) {
         return new Vector2D(
                 x * other.x,
                 y * other.y
         );
     }
 
-    public Vector2D divide(Vector2D other) {
+    public Vector2D divide(final Vector2D other) {
         return new Vector2D(
                 x / other.x,
                 y / other.y
