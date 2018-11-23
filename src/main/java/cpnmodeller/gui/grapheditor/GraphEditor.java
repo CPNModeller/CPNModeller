b395734617d2fc3aaaaacd2dc29745d2d9d2309b
Gerben-Vogelaar <Gerben.vogelaar@kpnmail.nl>
2018-11-23 13:04:49 +0100

    figuring out how it works

---
+++ src/main/java/cpnmodeller/gui/grapheditor/GraphEditor.java
@@ -0,0 +1,11 @@
+package cpnmodeller.gui.grapheditor;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphEditor extends JPanel {
+
+    public GraphEditor(){
+        this.setBackground(Color.RED);
+    }
+}
