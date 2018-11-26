8d27e411c285b6154464df91bdec1e3f77c742ca
Maarten Flippo <maartenflippo@outlook.com>
2018-11-26 15:34:00 +0100

    Added tokenmaker for standardml

--- src/main/java/cpnmodeller/gui/panels/texteditor/TextEditorPanel.java
+++ src/main/java/cpnmodeller/gui/panels/texteditor/TextEditorPanel.java
@@ -1,17 +1,26 @@
 package cpnmodeller.gui.panels.texteditor;
 
+import org.fife.ui.rsyntaxtextarea.AbstractTokenMakerFactory;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.TokenMakerFactory;
+import org.fife.ui.rtextarea.RTextScrollPane;
+
 import javax.swing.*;
 import java.awt.*;
 
 public final class TextEditorPanel extends JPanel {
 
-    private final JTextPane textPane;
+    private final RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
 
     public TextEditorPanel() {
-        textPane = new JTextPane(new StandardMLDocument());
-
         setLayout(new BorderLayout());
-        add(textPane, BorderLayout.CENTER);
+
+        AbstractTokenMakerFactory atmf = (AbstractTokenMakerFactory) TokenMakerFactory.getDefaultInstance();
+        atmf.putMapping("text/standardml", StandardMLTokenMaker.class.getName());
+        textArea.setSyntaxEditingStyle("text/standardml");
+
+        RTextScrollPane sp = new RTextScrollPane(textArea);
+        add(sp);
 
         setPreferredSize(new Dimension(600, 500));
     }
